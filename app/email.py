f04e8d9e85a27353436976df17840ef5b6c42e94
faiwong <981104072@qq.com>
2017-02-26 22:47:25 +0800

    Version 8h

--- app/email.py
+++ app/email.py
@@ -8,7 +8,8 @@ def send_async_email(app, msg):
 			mail.send(msg)
 			
 def send_email(to, subject, template, **kwargs):
-	msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, 
+	app = current_app._get_current_object()
+	msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject, 
 				  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
 	msg.body = render_template(template + '.txt', **kwargs)
 	msg.html = render_template(template + '.html', **kwargs)
