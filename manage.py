9b8765e5498b415297408bd95d9c1820648302f5
faiwong <981104072@qq.com>
2017-03-10 13:10:18 +0800

    Version 17b

--- manage.py
+++ manage.py
@@ -54,5 +54,15 @@ def profile(length=25, profile_dir=None):
     app.run()
     
     
+@manager.command
+def deploy():
+    """Run deployment tasks."""
+    from flask_migrate import upgrade
+    from app.models import Role, User
+    upgrade()
+    Role.insert_roles
+    User.add_self_follows()
+    
+    
 if __name__ == '__main__':
     manager.run()
